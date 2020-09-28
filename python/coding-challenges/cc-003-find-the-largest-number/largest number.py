4e66d09eba234f442d18e1cddcb9f480e96cdfce
ramsey <ramsey0179@gmail.com>
2020-09-28 22:08:52 +0300

    sprint2 sonu romen

---
+++ python/coding-challenges/cc-003-find-the-largest-number/largest number.py
@@ -0,0 +1,17 @@
+sayılar = []
+i = 0
+while i < 5:
+    try:
+        sayı = int(input("Bir sayı giriniz: "))
+    except:
+        print("Not Valid Input !!!")
+    else:
+        i += 1
+        sayılar.append(sayı)
+    print(i)
+büyük = sayılar[0]
+for i in sayılar:
+    if i > büyük:
+        büyük = i
+print(sayılar)
+print("Girdiğiniz sayılardan en büyüğü : ", büyük)
